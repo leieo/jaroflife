4953fb3756c81eb12e6d8f40bd9284372f72a6db
leieo <leila.el.omari@gmail.com>
2017-11-07 16:49:19 +0100

    create pdo

--- index.php
+++ index.php
@@ -6,16 +6,9 @@
   <body>
   	hello
   <?php
-  	/* Connecxion à une base ODBC avec l'invocation de pilote*/
-  	require 'config.php';
 
-  	$dsn = "mysql:dbname=$dbname;host=$host";
+  	require 'create-pdo.php';
 
-  	try {
-  		$pdo = new PDO($dsn, $user, $password);
-  	} catch (PDOException $e) {
-  		echo 'Connexion échouée : ' . $e->getMessage();
-    } 
   ?>
   </body>
 </html>
